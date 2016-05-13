f568455230e9c03c30e43acfbf5ace239a8db095
Antony Baasandorj <antony.baasan@gmail.com>
2016-05-13 11:40:19 -0600

    add create and edito methods

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,19 +11,38 @@ class ApplicationController < ActionController::API
   end
 
   def create
+    project = Project.new(project_params)
+
+    if project.save
+      render json: project
+    else
+      render json: project.errors, status: 422
+    end
   end
 
   def show
   end
 
   def update
+    project = Project.find(params[:id])
+
+    if project.update(project_params)
+      render json: project
+    else
+      render json: project.errors, status: 422
+    end
   end
 
   def destroy
+    project = Project.find(params[:id])
+    # Option A: just delete "project.destroy"
+    # Option B: just delete "project.archive" ()
+
+    head 204
   end
 
   private
-  def person_params
-    params.require(:@projects).permit(:name, :description, :author )
+  def project_params
+    params.require(:@projects).permit(:name, :description, :author, :content )
   end
 end
