f6674f01e0bc8ea9b68c33ff87d49dfcec03bdcb
Antony Baasandorj <antony.baasan@gmail.com>
2016-05-13 22:49:25 -0600

    fix show method

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,2 +1,55 @@
 class ProjectsController < ApplicationController
+  def count
+    render json: Project.count
+  end
+
+  def index
+
+    if (params[:search])
+      @projects = Project.where('name LIKE :search OR description LIKE :search OR author LIKE :search OR author_url LIKE :search', search: "%#{params[:search]}%").order('name')
+    else
+      # @projects = Project.all
+      @projects = Project.paginate(:page => params[:page], :per_page => 30).order('created_at DESC')
+    end
+
+    render json: @projects
+  end
+
+  def create
+    project = Project.new(project_params)
+
+    if project.save
+      render json: project
+    else
+      render json: project.errors, status: 422
+    end
+  end
+
+  def show
+    project = Project.find(params[:id])
+    render json: project
+  end
+
+  def update
+    project = Project.find(params[:id])
+
+    if project.update(project_params)
+      render json: project
+    else
+      render json: project.errors, status: 422
+    end
+  end
+
+  def destroy
+    project = Project.find(params[:id])
+    # Option A: just delete "project.destroy"
+    # Option B: just delete "project.archive" ()
+
+    head 204
+  end
+
+  private
+  def project_params
+    params.require(:project).permit(:name, :description, :author, :author_url, :content)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  get "projects/count", to: "projects#count"
   resources :projects
   resources :rats
+  get "projects/count", to: "projects#count"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
